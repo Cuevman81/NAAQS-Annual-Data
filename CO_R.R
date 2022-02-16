fd3c2986c8db5b7492e1d5d1e329283fd16fce12
Rodney Cuevas <80535587+Cuevman81@users.noreply.github.com>
2022-02-16 09:11:45 -0600

    Add files via upload

---
+++ CO_R.R
@@ -0,0 +1,61 @@
+#Installing or importing all packages
+
+library(expss)
+library(VIF)
+library(forecast)
+library(Metrics)
+library(DMwR2)
+library(dplyr)
+library(maps)
+library(ggplot2)
+library(aqsr)
+
+myuser <- create_user(email="",
+                      key="")
+
+#CO Summary
+
+
+CO <- aqs_annualData(aqs_user=myuser,
+                     endpoint="byState",
+                     state="28",
+                     bdate="20210101",
+                     edate="20211231",
+                     param="42101")
+
+dim(CO)
+
+write.csv(CO, file='CO.csv')
+
+newdataCO1H <- select(filter(CO, pollutant_standard == "CO 1-hour 1971"),c(sample_duration,second_max_value,local_site_name,second_max_datetime,latitude,longitude))
+write.csv(newdataCO1H, file="CO1HR.csv")
+
+newdataCO8H <- select(filter(CO, pollutant_standard == "CO 8-hour 1971"),c(sample_duration,second_max_value,local_site_name,second_max_datetime,latitude,longitude))
+write.csv(newdataCO8H, file="CO8HR.csv")
+
+#1 Hour CO
+ggplot(newdataCO1H, aes(x = local_site_name , newdataCO1H, aes(x = local_site_name))) +
+  geom_point(aes(y = second_max_value, color = "1-Hour"), size = 3) +
+  xlab("Site")+ylab("CO1HR") +
+  labs(title="1 and CO")
+
+#8 Hour CO
+ggplot(newdataCO8H, aes(x = local_site_name , newdataCO8H, aes(x = local_site_name))) +
+  geom_point(aes(y = second_max_value, color = "8-Hour"), size = 3) +
+  xlab("Site")+ylab("CO8HR") +
+  labs(title="8 Hour CO")
+
+
+#Statewide Map of 1-HR CO(PPB)
+map(database = 'county','mississippi')
+newdataCO1H <- read.csv("CO1HR.csv")
+points(x = newdataCO1H$longitude, y = newdataCO1H$latitude, pch = 19, col = 'blue')
+title(main = "1-HR CO")
+text(newdataCO1H$longitude, y = newdataCO1H$latitude, labels = newdataCO1H$second_max_value, pos = 2, font = 2, cex = 1.5)
+
+#Statewide Map of 8-HR CO(PPB)
+map(database = 'county','mississippi')
+newdataCO8H <- read.csv("CO8HR.csv")
+points(x = newdataCO8H$longitude, y = newdataCO8H$latitude, pch = 19, col = 'blue')
+title(main = "8-HR CO")
+text(newdataCO8H$longitude, y = newdataCO8H$latitude, labels = newdataCO8H$second_max_value, pos = 2, font = 2, cex = 1.5)
---
+++ SO2R.R
@@ -0,0 +1,62 @@
+#Installing or importing all packages
+
+library(expss)
+library(VIF)
+library(forecast)
+library(Metrics)
+library(DMwR2)
+library(dplyr)
+library(maps)
+library(ggplot2)
+library(aqsr)
+
+myuser <- create_user(email="",
+                      key="")
+
+
+#S02 Summary
+
+
+SO2 <- aqs_annualData(aqs_user=myuser,
+                      endpoint="byState",
+                      state="28",
+                      bdate="20200101",
+                      edate="20201231",
+                      param="42401")
+
+dim(SO2)
+
+write.csv(SO2, file='SO2.csv')
+
+newdataSO21H <- select(filter(SO2, pollutant_standard == "SO2 1-hour 2010"),c(sample_duration,local_site_name,ninety_ninth_percentile,latitude,longitude))
+write.csv(newdataSO21H, file="SO21HR.csv")
+
+newdataSO23H <- select(filter(SO2, pollutant_standard == "SO2 3-hour 1971"),c(sample_duration,second_max_value,local_site_name,second_max_datetime,latitude,longitude))
+write.csv(newdataSO23H, file="SO23HR.csv")
+
+#1 Hour SO2
+ggplot(newdataSO21H, aes(x = local_site_name , newdataSO21H, aes(x = local_site_name))) +
+  geom_point(aes(y = ninety_ninth_percentile, color = "1-Hour"), size = 3) +
+  xlab("Site")+ylab("SO21HR") +
+  labs(title="1-Hour SO2")
+
+#3 Hour SO2
+ggplot(newdataSO23H, aes(x = local_site_name , newdataSO23H, aes(x = local_site_name))) +
+  geom_point(aes(y = second_max_value, color = "3-Hour"), size = 3) +
+  xlab("Site")+ylab("SO23HR") +
+  labs(title="3-Hour SO2")
+
+
+#Statewide Map of 1-HR SO2(PPB)
+map(database = 'county','mississippi')
+newdataSO21H <- read.csv("SO21HR.csv")
+points(x = newdataSO21H$longitude, y = newdataSO21H$latitude, pch = 19, col = 'blue')
+title(main = "1-HR SO2")
+text(newdataSO21H$longitude, y = newdataSO21H$latitude, labels = newdataSO21H$ninety_ninth_percentile, pos = 2, font = 2, cex = 1.5)
+
+#Statewide Map of 3-HR SO2(PPB)
+map(database = 'county','mississippi')
+newdataCO8H <- read.csv("SO23HR.csv")
+points(x = newdataSO23H$longitude, y = newdataSO23H$latitude, pch = 19, col = 'blue')
+title(main = "3-Hour SO2")
+text(newdataSO23H$longitude, y = newdataSO23H$latitude, labels = newdataSO23H$second_max_value, pos = 2, font = 2, cex = 1.5)
