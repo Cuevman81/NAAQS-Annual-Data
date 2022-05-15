52862b378c9f5222724ac60fc4958af60f6377ae
Rodney Cuevas <80535587+Cuevman81@users.noreply.github.com>
2022-05-15 11:52:14 -0500

    Update PM25_R.R
    Added Daily 24 Hour Average Plots.

--- PM25_R.R
+++ PM25_R.R
@@ -56,3 +56,76 @@ PM25Annual <- read.csv("PM25Annual.csv")
 points(x = PM25Annual$longitude, y = PM25Annual$latitude, pch = 19, col = 'blue')
 title(main = "24-Hour PM2.5")
 text(PM25Annual$longitude, y = PM25Annual$latitude, labels = PM25Annual$ninety_eighth_percentile, pos = 3, font = 2)
+
+#Daily PM2.5 Data for Sites Across the State of Mississippi
+download.file("https://aqs.epa.gov/aqsweb/airdata/daily_88101_2020.zip",
+              destfile="daily_88101_2020.zip")
+
+unzip("daily_88101_2020.zip")
+p = read.csv("daily_88101_2020.csv")
+
+p2 <- select(filter(p, Pollutant.Standard == "PM25 24-hour 2012"),c(Sample.Duration,Date.Local,Arithmetic.Mean,Local.Site.Name,Latitude,Longitude,State.Name))
+write.csv(p2, file="PM25Daily.csv")
+
+names(p2)
+
+p2 = p2[p2$State.Name == "Mississippi",]
+
+head(p2)
+
+p2$Date.Local = as.Date(p2$Date.Local)
+
+plot( factor(p2$Local.Site.Name), p2$Arithmetic.Mean )
+
+unique(p2$Local.Site.Name)
+
+
+# Linear trends an summaries
+ggplot( p2[p2$Local.Site.Name =="Cleveland Delta State",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Cleveland Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Cleveland Delta State",])
+
+ggplot( p2[p2$Local.Site.Name =="Gulfport Youth Court",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Gulfport Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Gulfport Youth Court",])
+
+ggplot( p2[p2$Local.Site.Name =="Hernando",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Hernando Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Hernando",])
+
+ggplot( p2[p2$Local.Site.Name =="Hinds CC",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Hinds CC Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Hinds CC",])
+
+ggplot( p2[p2$Local.Site.Name =="Jackson NCORE",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Jackson NCORE Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Jackson NCORE",])
+
+ggplot( p2[p2$Local.Site.Name =="Pascagoula",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Pascagoula Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Pascagoula",])
+
+ggplot( p2[p2$Local.Site.Name =="Waveland",], aes(x = Date.Local, y = Arithmetic.Mean)) +
+  geom_point(aes(color = Arithmetic.Mean), size = 2) +
+  geom_smooth() +
+  labs(title="Waveland Daily 24 Hour Average PM2.5") +
+  theme(axis.text.x=element_text(angle=70, hjust=1))
+summary (p2[p2$Local.Site.Name == "Waveland",])
